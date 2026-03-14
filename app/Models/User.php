80e13975d25519a118128b474bcd1d74527ea2a0
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2026-03-14 21:17:49 +0000

    New APP

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,4 +30,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function setting()
+    {
+        return $this->hasOne(Setting::class);
+    }
 }
