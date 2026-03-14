39d4ecd646a98fc47a69352d54e968c50e233dfd
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2026-03-14 21:20:20 +0000

    New APP

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'google_review_url',
+        'reply_to_email',
+        'email_signature'
+    ];
 }
